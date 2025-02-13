dd718f2b4b3496fa3ad69babf980c5d39c94965e
mitermitek <188499468+mitermitek@users.noreply.github.com>
2025-02-13 23:08:29 +0100

    feat: add primary key constraint to user_client table

--- database/migrations/2025_02_13_202540_create_user_client_table.php
+++ database/migrations/2025_02_13_202540_create_user_client_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_client', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained();
             $table->foreignId('client_id')->constrained();
+            $table->primary(['user_id', 'client_id']);
         });
     }
 
