e89f7445c822c1950adb05cfb240d706b276a89d
mitermitek <188499468+mitermitek@users.noreply.github.com>
2025-02-13 21:38:35 +0100

    feat: add Template model and migration with relationship to Client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -15,4 +15,9 @@ class Client extends Model
     {
         return $this->belongsToMany(User::class);
     }
+
+    public function templates()
+    {
+        return $this->hasMany(Template::class);
+    }
 }
---
+++ app/Models/Template.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Template extends Model
+{
+    protected $fillable = [
+        'name'
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+}
